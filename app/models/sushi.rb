60f5b93452e809fb3a9d04bde7ecb8fbc4d63cc0
cjbrock <corinna@flatironschool.com>
2020-05-12 14:46:51 -0400

    post study group code

--- app/models/sushi.rb
+++ app/models/sushi.rb
@@ -1,4 +1,15 @@
 class Sushi < ApplicationRecord
-  validates :name, presence: true
-  validates :name, uniqueness: true
+  # validates :name, presence: true
+  # validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validate :starts_with_x
+
+  private
+
+    def starts_with_x
+      if name[0].downcase == "x"
+        errors.add(:x, "cannot be the first letter")
+      end
+    end
+
 end
---
+++ validation-notes.md
@@ -0,0 +1,17 @@
+# What are validations?
+ - allow us to have some kind of error checking 
+ - user authentication, valid inputs, presence, uniqueness, almost anything
+
+ https://guides.rubyonrails.org/active_record_validations.html
+
+ - run before we persist our data
+
+ ## What methods run validations?
+
+ - save, valid?, invalid?, create, update
+
+ ## What don't?
+
+ - new, build
+
+ 
